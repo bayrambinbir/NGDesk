6bbd88414d13979d2f41430586afc16031d16331
bayram1001 <email@email.com>
2018-07-28 13:06:52 -0500

    NGDesk

---
+++ src/main/java/com/NGDesk/com/NGDesk/App.java
@@ -0,0 +1,13 @@
+package com.NGDesk.com.NGDesk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
