36212ec85429577d30f01fbe8b1009377cde598e
aslisahin <a_turkal@yahoo.com>
2018-08-02 13:21:32 -0500

    first case of Asli is done

--- src/test/java/com/NGDesk/tests/login/TicketsPageTests.java
+++ src/test/java/com/NGDesk/tests/login/TicketsPageTests.java
@@ -2,7 +2,12 @@ package com.NGDesk.tests.login;
 
 import static org.testng.Assert.assertEquals;
 
-import org.testng.annotations.BeforeMethod;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import com.NGDesk.pages.HomePage;
@@ -10,6 +15,7 @@ import com.NGDesk.pages.LoginPage;
 import com.NGDesk.pages.TicketPage;
 import com.NGDesk.tests.TestBase;
 import com.NGDesk.utilities.ConfigurationReader;
+import com.NGDesk.utilities.BrowserUtils;
 
 public class TicketsPageTests extends TestBase {
 
@@ -33,4 +39,72 @@ public class TicketsPageTests extends TestBase {
 		
 	}
 
+	@Test(groups= {"deneme"})
+	public void viewCreatedTicket() throws InterruptedException {
+		driver.get(ConfigurationReader.getProperty("loginDirectPageUrl"));
+		loginPage.username.sendKeys(ConfigurationReader.getProperty("usernamea"));
+		loginPage.password.sendKeys(ConfigurationReader.getProperty("passworda"));
+		loginPage.loginButton.click();
+		
+		driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(ticketPage.hoverTicketButton).perform();
+		BrowserUtils.waitForVisibility(ticketPage.newticket, 5);
+		ticketPage.newticket.click();
+		
+		String expectedRequesterTab = "asli turk";
+		BrowserUtils.waitForVisibility(ticketPage.requesterTab, 5);
+		String actualRequesterTab = ticketPage.requesterTab.getText();
+		
+		System.out.println("actualRequesterTab : "  + actualRequesterTab);
+		assertEquals(actualRequesterTab, expectedRequesterTab);
+		
+		ticketPage.severityTabClick.click();
+		ticketPage.severityTabLow.click();
+		
+		BrowserUtils.waitFor(5);
+
+		ticketPage.teamTabClick.click();
+		ticketPage.teamTabDefault.click();
+		
+		ticketPage.subject.sendKeys("Testing new ticket 123");
+		
+		actions = new Actions(driver);
+		actions.moveToElement(ticketPage.body);
+		actions.click();
+		actions.sendKeys("Being automation tester is cool");
+		actions.build().perform();
+		
+		actions = new Actions(driver);
+		BrowserUtils.waitForVisibility(ticketPage.ccUsers, 5);
+		actions.moveToElement(ticketPage.ccUsers);
+		actions.click();
+		actions.sendKeys("asat1121");
+		actions.build().perform();
+		actions.moveToElement(ticketPage.asat1121).click().build().perform();
+		
+		ticketPage.submit.click();
+		
+		String expectedConfirmationText = "Confirm New Ticket";
+		String actualConfirmationText = ticketPage.confirmationNewTicket.getAttribute("innerHTML");
+		assertEquals(actualConfirmationText, expectedConfirmationText);
+		
+		actions = new Actions(driver);
+		BrowserUtils.waitForVisibility(ticketPage.saveAndSubmit, 5);
+		actions.moveToElement(ticketPage.saveAndSubmit);
+		actions.click();
+		actions.build().perform();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
